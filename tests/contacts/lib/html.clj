50adf0a60981b63232f69a3e0c9f9c2f88e408e1
Hugh Powell <hugh@hughpowell.co.uk>
2023-07-08 18:39:05 +1000

    Partition based on the number of headers
    rather than hard coding.

--- tests/contacts/lib/html.clj
+++ tests/contacts/lib/html.clj
@@ -5,5 +5,5 @@
   (->>
     (enlive/select snippet #{[:table :tr :> enlive/void] [:table :tr :> :td :> enlive/text-node]})
     (map (fn [node] (when (string? node) node)))
-    (partition 4)
+    (partition (count headers))
     (map (fn [contact] (into {} (map #(vector %1 (str %2)) headers contact))))))
