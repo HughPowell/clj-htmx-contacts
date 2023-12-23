64a87b577d191a10730822319a1c494b57e50e1c
Hugh Powell <hugh@hughpowell.co.uk>
2023-12-23 20:42:34 +1100

    Store secrets in Hashicorp Vault

--- dev/user.clj
+++ dev/user.clj
@@ -1,6 +1,9 @@
 (ns user
-  (:require [clojure.repl]
+  (:require [aero.core :as aero]
+            [clojure.java.shell :as shell]
+            [clojure.repl]
             [clojure.repl.deps]
+            [clojure.string :as string]
             [com.stuartsierra.component :as component]
             [com.stuartsierra.component.repl :as component.repl]
             [contacts.system.app :as app]
@@ -16,6 +19,29 @@
   [clojure.repl doc]
   [clojure.repl.deps sync-deps]
   [com.stuartsierra.component.repl reset])
+
+(defn sh [& args]
+  (let [{:keys [exit out err] :as response} (apply shell/sh args)]
+    (if (zero? exit)
+      (string/trimr out)
+      (throw (ex-info (if (seq err) err out) response)))))
+
+(defn load-secrets []
+  (sh "vlt" "login")
+  (->> (sh "vlt" "secrets" "list")
+       (string/split-lines)
+       (rest)
+       (pmap (fn [line]
+               (let [secret-name (re-find #"^\w+" line)]
+                 [secret-name
+                  (sh "vlt" "secrets" "get" "-plaintext" secret-name)])))
+       (into {})))
+
+(defonce secrets (load-secrets))
+
+(defmethod aero/reader 'vault
+  [_opts _tag value]
+  (get secrets (str value)))
 
 (component.repl/set-init
   (fn [_]
@@ -33,4 +59,6 @@
 (comment
   (sync-deps)
   (reset)
+
+  (alter-var-root #'secrets (constantly (load-secrets)))
   )
